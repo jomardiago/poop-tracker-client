525760a5f8cb2cd8dbe6005874d1fb773f5347b4
Jose <Jose.Diago@kyndryl.com>
2023-12-08 08:18:16 +0800

    Avatar, Logout

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,9 +1,42 @@
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 import poopLogo from "@/assets/poop-black.svg";
+import useSessionStore from "@/stores/session-store";
+import { LogOut } from "lucide-react";
 
 export const Header = () => {
+  const { session, setSession } = useSessionStore();
+
+  const logout = () => setSession(undefined);
+
   return (
-    <nav className="h-14 border-b shadow-sm flex items-center px-4">
+    <nav className="h-14 border-b shadow-sm flex justify-between items-center px-4">
       <img src={poopLogo} alt="Poop logo" className="w-12 h-12" />
+
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <Avatar asChild>
+            <AvatarFallback className="bg-stone-500 text-white uppercase">
+              {session?.username.substring(0, 2)}
+            </AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>{session?.email}</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem className="cursor-pointer" onClick={logout}>
+            <LogOut className="w-4 h-4 mr-2" /> Logout
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </nav>
   );
 };
--- src/layouts/main/main-layout.tsx
+++ src/layouts/main/main-layout.tsx
@@ -1,6 +1,20 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 import { Header } from "@/components/header/header";
+import useSessionStore from "@/stores/session-store";
+import PATHS from "@/lib/paths";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
+  const { session } = useSessionStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!session) {
+      navigate(PATHS.login);
+    }
+  }, [session, navigate]);
+
   return (
     <div>
       <Header />
