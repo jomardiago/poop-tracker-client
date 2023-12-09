1fd7c081b8268d9a3bb812e44b89e03760bbe7cf
Jose <jomardiago@gmail.com>
2023-12-09 20:47:26 +0800

    Using the production api url

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://poop-tracker-server.onrender.com",
 });
 
 export const getRequestHeader = () => {
